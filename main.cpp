75a2627a4d5156886cc087e3537d5de91e2b1278
carsonclouse <92872734+carsonclouse@users.noreply.github.com>
2023-12-02 18:21:05 -0500

    Update main.cpp- changing return types and variables for sorts

--- main.cpp
+++ main.cpp
@@ -96,20 +96,21 @@ int main()
     return 0;
 }
 
+
 /*
 //basic merge sort function
-string <pair<string, string>> mergeSort(string <pair<string, string>> map) { //edit based on type of map used and return type
+unordered_multimap<string, weatherData> mergeSort(unordered_multimap<string, weatherData> map) {
     if length(map) < = 1;
         return map;
     int mid = length(map)/2; //divide map in half and recursively sort each half
-    string<pair<string, string>> left = mergeSort(); //recursively sort the left half of data
-    string<pair<string, string>> right = mergeSort(); //recursively sort the right half of data
+    unordered_multimap<string, weatherData> left = mergeSort(); //recursively sort the left half of data
+    unordered_multimap<string, weatherData> right = mergeSort(); //recursively sort the right half of data
     return merge(left, right);
 }
 
 //merge function for merge sort
-string<pair<string, string>> merge(string<pair<string, string>> left, string<pair<string, string>> right) {
-    string <pair<string, string>> result; //create empty map to store results
+unordered_multimap<string, weatherData> merge(unordered_multimap<string, weatherData> left, unordered_multimap<string, weatherData> right) {
+    unordered_multimap<string, weatherData> result; //create empty map to store results
     while(!left.empty() && !right.empty()) {
         if (left[0] < right[0]) {
             result.append(left[0]);
@@ -127,7 +128,7 @@ string<pair<string, string>> merge(string<pair<string, string>> left, string<pai
 
 
 //basic quick sort function
-string<pair<string, string>> quickSort (string<pair<string, string>> map, int low, int high) {
+unordered_multimap<string, weatherData> quickSort (unordered_multimap<string, weatherData> map, int low, int high) {
     if (low < high) {
         int pivotIndex = partition(map, low, high);
         quickSort(map, low, pivotIndex-1); //recursively sort the left section
@@ -137,8 +138,8 @@ string<pair<string, string>> quickSort (string<pair<string, string>> map, int lo
 }
 
 //partition function for quick sort
-int partition (string<pair<string, string>> map, int low, int high) { //edit all based on variable names and return types
-    pair<string, string> pivot = map[high]; //choose the rightmost element as pivot
+int partition (unordered_multimap<string, weatherData> map, int low, int high) {
+    string pivot = map[high]; //choose the rightmost element as pivot
     int left = low;
     int right = high - 1;
     while (true) {
@@ -155,4 +156,5 @@ int partition (string<pair<string, string>> map, int low, int high) { //edit all
     }
     swap(map[left], map[high]) //move pivot to the middle
     return left; //return partitioning index
-}*/
+}
+*/
